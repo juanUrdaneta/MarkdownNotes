a40231077601fd8d72c3b6e4fb7e39b8cbedde26
Juan <juankorus@gmail.com>
2020-06-22 23:35:08 -0300

    added cheat sheet link

--- src/Components/NoteView.jsx
+++ src/Components/NoteView.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import marked from 'marked';
 import { useSpring, animated } from 'react-spring';
 
-const DEFAULT_TEXT  = "# Hello! \n\n## This app store your notes in the browser.\n\n---\n\nWith this app you can write notes with Markdown format and store them in the browser for future reference.\n\n* You can use *most* of the available markup functions\n* You can **highlight** important text\n* Create lists\n* `And even add a code block!`\n\n> You could Quote an important note\n\n---\n\n**Start by creating a new note in the side menu!** \n\n Check the Cheat Sheet"
+const DEFAULT_TEXT  = "# Hello! \n\n## This app store your notes in the browser.\n\n---\n\nWith this app you can write notes with Markdown format and store them in the browser for future reference.\n\n* You can use *most* of the available markup functions\n* You can **highlight** important text\n* Create lists\n* `And even add a code block!`\n\n> You could Quote an important note\n\n---\n\n**Start by creating a new note in the side menu!** \n\n [Check the Cheat Sheet](https://www.markdownguide.org/cheat-sheet/)"
 
 const NoteView = ({item, isEditing}) => {
   const viewer = useRef(null);
